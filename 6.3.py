dbdf618b4a0deb5ff94706c6d56af203c3734efa
Feodora <feodora.obr@gmail.com>
2020-07-09 00:00:08 +0200

    Update 6.3.py

--- 6.3.py
+++ 6.3.py
@@ -1,10 +1,10 @@
 import turtle
 
-def drawPoly(turtle, sides, size):
-    """Make turtle someturtle draw a square with somesides somesize."""
-    for i in range(sides):
+def drawPoly(turtle, n, size):
+    """Make turtle draw a polygon with n-sides of size."""
+    for i in range(n):
         turtle.forward(size)
-        turtle.left(360/sides)
+        turtle.left(360/n)
 
 wn = turtle.Screen()       # set up the window and its attributes
 wn.bgcolor("lightyellow")

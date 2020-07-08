1717f1a470d3e0f5c03147b7d2a7b4df3136ce20
Feodora <feodora.obr@gmail.com>
2020-07-08 23:53:49 +0200

    Update 6.5.py
    combine two functions into one

--- 6.5.py
+++ 6.5.py
@@ -1,23 +1,17 @@
 import turtle
 
-def drawSpiral(t, sz):
-    """Make turtle t draw a spiral with line length sz."""
-    t.forward(-sz)
-    t.left(90)
-    t.forward(sz)
-    t.left(90)
-        
-def growingSpiral(x):
-    size = 3
-    for counter in range(x):
-        size = size + 5
-        drawSpiral(alex, size)    # call the function to draw the square        
-        
+def drawSpiral(t, angle):
+    """Make turtle t draw a spiral with angle."""
+    line_length = 3
+    t.forward(line_length)
+    t.left(angle)
+    line_length = line_length + 5
+           
 wn = turtle.Screen()              # set up the window and its attributes
 wn.bgcolor("lightyellow")
 
 alex = turtle.Turtle()            # create alex
 alex.color('darkolivegreen')
 
-growingSpiral(50)
+drawSpiral(alex, 90)        
 wn.exitonclick()

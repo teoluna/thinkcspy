553d4796c69370f8a67bb9ca7d1093f4eb7d77f2
Feodora <feodora.obr@gmail.com>
2020-07-11 14:02:01 +0200

    Correction for negative values

--- turtle_barchart.py
+++ turtle_barchart.py
@@ -2,42 +2,48 @@ import turtle
 
 def drawBar(t, height):
     """ Get turtle t to draw one bar, of height. """
-    t.begin_fill()               
+    t.begin_fill()
+    if height < 0: # move to write the negative value of data
+        t.penup()
+        t.left(90)
+        t.forward(5)
+        t.write(str(height))
+        t.backward(5)
+        t.right(90)
+        t.pendown()
     t.left(90)
     t.forward(height)
+    if height >= 0: # move to write the positive value of data
+        t.penup()
+        t.forward(5)
+        t.write(str(height))
+        t.backward(5)
+        t.pendown()
     t.right(90)
     t.forward(40)
     t.right(90)
     t.forward(height)
-    t.end_fill()
-    t.penup()          # move to write the value of the data
-    t.backward(height)
-    t.right(90)
-    t.forward(20)
-    t.right(90)
-    t.forward(5)
-    t.write(str(height))
-    t.forward(-5)
-    t.right(90)
-    t.forward(20)
-    t.right(90)
-    t.forward(height)
     t.left(90)
-    t.pendown()
+    t.end_fill()
 
 
-xs = [48, 117, 200, 240, 160, 260, 220]  # here is the data
+xs = [-48, 117, 200, 240, -160, 260, 220]  # here is the data
 maxheight = max(xs)
+minheight = min(xs)
 numbars = len(xs)
 border = 10
 
+if minheight < 0:
+    par = minheight - border
+else:
+    par = 0
+
 wn = turtle.Screen()             # set up the window and its attributes
-wn.setworldcoordinates(0-border, 0-border, 40*numbars+border, maxheight+border+5)
+wn.setworldcoordinates(0-border, par, 40*numbars+border, maxheight+border+5)
 wn.bgcolor("lightgreen")
 
 tess = turtle.Turtle()           # create tess and set some attributes
 tess.pensize(3)
-
 
 for a in xs:                  # draw a bar chart
     if a >= 200:

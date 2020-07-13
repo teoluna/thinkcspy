9974280bbe609c6123dd0cde60fdb4cbbd6e80fe
Feodora <feodora.obr@gmail.com>
2020-07-13 20:24:25 +0200

    Update 8.5.py

--- 8.5.py
+++ 8.5.py
@@ -27,15 +27,30 @@ def isInScreen(w, t):
     turtleY = t.ycor()
 
     stillIn = True
-    if turtleX > rightBound or turtleX < leftBound:
+    if turtleX >= rightBound or turtleX <= leftBound:
         stillIn = False
-    if turtleY > topBound or turtleY < bottomBound:
+    if turtleY >= topBound or turtleY <= bottomBound:
         stillIn = False
 
     return stillIn
 
-while isInScreen(wn, t1) and isInScreen(wn, t2):
-    move_randomly(wn, t1)
-    move_randomly(wn, t2)
+def areColliding(t1, t2):
+    if t1.distance(t2) < 2:
+        return True
+    else:
+        return False
 
-wn.exitonclick()
+t1.forward(150)
+t2.backward(150)
+
+while (isInScreen(wn, t1) or isInScreen(wn, t2)) and not areColliding(t1, t2):
+    if not isInScreen(wn, t1):
+        t1.backward(150)
+    else:
+        move_randomly(wn, t1)
+    if not isInScreen(wn, t2):
+        t2.backward(150)
+    else:
+        move_randomly(wn, t2)
+
+wn.exit()

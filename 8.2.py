7c50e74263a89690a6d969c4ca730c99268fbdd4
Feodora <feodora.obr@gmail.com>
2020-07-13 14:14:04 +0200

    solution with while instead of for

--- 8.2.py
+++ 8.2.py
@@ -1,6 +1,8 @@
 def print_triangular_numbers(n):
-    for i in range(1, n+1):  # generate values for columns
+    i = 1
+    while i < n + 1:
         t = int((i*(i+1))/2)
         print(i, "\t", t)
+        i = i + 1
 
 print_triangular_numbers(5)

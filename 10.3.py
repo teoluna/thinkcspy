b9921c7d77235e9c4261ecbec9474e8bedbd56ee
Feodora <feodora.obr@gmail.com>
2020-08-19 10:08:46 +0200

    Create 10.3.py

---
+++ 10.3.py
@@ -0,0 +1,14 @@
+myList = [76, 92.3, 'hello', True, 4, 76]
+
+myList.append('apple')
+myList.append(76)
+myList.insert(3, 'cat')
+myList.insert(0, 99)
+print(myList)
+
+print(myList.index('hello'))
+print(myList.count(76))
+myList.remove(76)
+myList.pop(myList.index(True))
+
+print(myList)

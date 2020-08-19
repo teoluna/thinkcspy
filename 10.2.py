a907c2f9322725e46b6dc6f93223fbcbc52d31da
Feodora <feodora.obr@gmail.com>
2020-08-19 09:57:05 +0200

    Create 10.2.py

---
+++ 10.2.py
@@ -0,0 +1,21 @@
+#create myList with append method
+myList = []
+myList.append(76)
+myList.append(92.3)
+myList.append("hello")
+myList.append(True)
+myList.append(4)
+myList.append(76)
+
+print(myList)
+
+#create myList with concatenation
+myList = []
+myList = myList + [76]
+myList = myList + [92.3]
+myList = myList + ["hello"]
+myList = myList + [True]
+myList = myList + [4]
+myList = myList + [76]
+
+print(myList)

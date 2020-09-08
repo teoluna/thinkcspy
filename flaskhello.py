dd51c16438ca4305282023e34f486c27260c98c2
Feodora <feodora.obr@gmail.com>
2020-09-08 15:02:00 +0200

    Update flaskhello.py

--- flaskhello.py
+++ flaskhello.py
@@ -1,21 +1,35 @@
-from flask import Flask
+from flask import Flask, request
 from datetime import datetime
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return """<html><body>
-        <h1>Hello, world!</h1>
-        Click <a href='/red'>here</a> for the text in red.
-        The time is {0}.</body></html>""".format(
-            str(datetime.now()))
+def home():
+    return """
+        <html><body>
+            <h2>Welcome to the Greeter</h2>
+            <form action="/greet">
+                What's your name? <input type='text' name='username'><br>
+                What's your favorite food? <input type='text' name='favfood'><br>
+                <input type='submit' value='Continue'>
+            </form>
+        </body></html>"""
 
-@app.route('/red')
-def hello_red():
-    return """<html><body>
-    <h2 style='color:red'>Hello, world!</h2>
-    </html></body>"""
+@app.route('/greet')
+def greet():
+    username = request.args.get('username', 'World')
+    favfood = request.args['favfood']
+    if favfood == '':
+        msg = 'You did not tell me your favorite food.'
+    else:
+        msg = 'I like ' + favfood + ', too.'
+
+    return """
+        <html><body>
+            <h2 style='color:red'>Hello, {0}!</h2>
+            {1}
+        </html></body>
+        """.format(username, msg)
 
 # Launch the Flask dev server
-app.run(host="0.0.0.0", port=80, debug=True)
+app.run(host="localhost", debug=True)
